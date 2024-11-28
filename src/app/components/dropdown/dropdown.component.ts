c375243545b0a4cb75f7a2dcbc6e04c93afcf1b8
Mateus Robers <robersmateus@gmail.com>
2024-11-28 08:22:28 -0300

    upando o projeto

---
+++ src/app/components/dropdown/dropdown.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.scss'],
+})
+export class DropdownComponent {
+  isOpen = false;
+
+  toggleDropdown(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeDropdown(): void {
+    this.isOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event): void {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown')) {
+      this.closeDropdown();
+    }
+  }
+}
