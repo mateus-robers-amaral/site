9c0a15ebfc2c943626f7c9efd04e6ecaf0d2a1be
Mateus Robers <robersmateus@gmail.com>
2024-11-28 10:37:25 -0300

    wip: atualizando o projeto e adicionando components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { BotaoComponent } from "../botao/botao.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, BotaoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { DropdownComponent } from '../dropdown/dropdown.component';
+import { CommonModule } from '@angular/common';
+import { BotaoComponent } from "../botao/botao.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [DropdownComponent],
+  imports: [DropdownComponent, CommonModule, BotaoComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // A condição verifica a quantidade de rolagem em pixels
+    if (window.scrollY > 0) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 }
